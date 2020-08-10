f3de1dd1d33c39bd9dfcc82e5bca6cc53c9794ba
Patrice LaFlamme <patrix@patrix.org>
2020-08-10 16:23:39 +0900

    better(?) tests

--- test_binary.py
+++ test_binary.py
@@ -10,26 +10,22 @@ def test_make_column():
     assert make_column(100, red) == [black, red, red, black, black, red, black, black]
     assert make_column(100, blue) == [black, blue, blue, black, black, blue, black, black]
 
-def test_temperature_color():
-    assert temperature_to_color(19) == blue
-    assert temperature_to_color(21) == green
-    assert temperature_to_color(26) == red
-    assert temperature_to_color(40) == red
+def test_make_temperatures():
+    assert make_temperature(24) == [black, black, black, green, green, black, black, black]
+    assert make_temperature(19) == [black, black, black, blue, black, black, blue, blue]
+    assert make_temperature(35) == [black, black, red, black, black, black, red, red]
 
-def test_humidity_color():
-    assert humidity_to_color(49) == red
-    assert humidity_to_color(50) == green
-    assert humidity_to_color(60) == blue
-
-def test_pressure_color():
-    assert pressure_to_color(1000) == green
-    assert pressure_to_color(980) == red
-    assert pressure_to_color (1050) == blue
-
-def test_make_columns():
-    assert make_temperature(25) == [black, black, black, red, red, black, black, red]
+def test_make_humidities():
+    assert make_humidity(45) == [black, black, red, black, red, red, black, red]
     assert make_humidity(55) == [black, black, green, green, black, green, green, green]
+    assert make_humidity(75) == [black, blue, black, black, blue, black, blue, blue]
+
+def test_make_pressures():
+    assert make_pressure(999) == [black, black, black, black, black, black, black, red]
     assert make_pressure(1013) == [black, black, black, black, green, green, black, green]
+    assert make_pressure(1053) == [black, black, blue, blue, black, blue, black, blue]
+
+def test_make_time():
     assert make_hour(15) == [black, black, black, black, yellow, yellow, yellow, yellow]
     assert make_minute(55) == [black, black, red, red, black, red, red, red]
     assert make_second(18) == [black, black, black, blue, black, black, blue, black]
